f476a96281300d013791b8a845594bd2f16996ff
duongbui123 <buiducduong2342000@gmail.com>
2021-05-27 01:04:25 +0700

    validate data and create custom validator

--- 2-natours/model/tourModel.js
+++ 2-natours/model/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,15 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [
+        40,
+        'A tour name must have less or equal then 40  characters',
+      ],
+      minlength: [
+        10,
+        'A tour name must have more or equal then 40  characters',
+      ],
+      validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     secretTour: {
@@ -25,10 +35,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either : easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1.0, 'A rating must be above 1.0'],
+      max: [5.0, 'A rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -38,7 +54,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        // this KEYWORD ONLY RUNS WHEN CREATE NEW DOC
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
